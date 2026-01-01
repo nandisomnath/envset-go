3f60e9e07d984e8edaa11529a229341f723abb6a
Somnath Nandi <somnathnandi368@gmail.com>
2026-01-02 01:38:08 +0530

    addpath and remove path implemetation for fish shell

--- core/fish_shell.go
+++ core/fish_shell.go
@@ -1,9 +1,11 @@
 package core
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"path"
+	"strings"
 )
 
 type Shell interface {
@@ -18,7 +20,7 @@ type Shell interface {
 type FishShell struct{}
 
 func (sh FishShell) GetShellCode(env_value string) string {
-	return fmt.Sprintf("set -x PATH %s $PATH", env_value)
+	return fmt.Sprintf("set -x PATH %s $PATH\n", env_value)
 }
 
 func (sh FishShell) InitSetup() {
@@ -44,17 +46,69 @@ func (sh FishShell) InitSetup() {
 
 
 
-func (sh FishShell) AddPath()    {
-	
+func (sh FishShell) AddPath(env_value string)    {
+	user_conf_path := sh.GetUserConfigPath()
+	user_conf_file, err := os.OpenFile(user_conf_path, os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err.Error())
+	}
+	shell_code := sh.GetShellCode(env_value)
+	_, err = user_conf_file.WriteString(shell_code)
+	if err != nil {
+		panic(err.Error())
+	}
 }
-func (sh FishShell) RemovePath() {}
+
+func (sh FishShell) RemovePath(env_value string) {
+	// all variables
+	user_conf_path := sh.GetUserConfigPath()
+	shellCode := sh.GetShellCode(env_value)
+
+	// copy all strings in tmp file
+	tmpDir := os.TempDir()
+	tmpFile, err := os.CreateTemp(tmpDir, "envset")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer tmpFile.Close()
+	tmpWriter := bufio.NewWriter(tmpFile)
+	userConfFileReaderFile, err := os.Open(user_conf_path)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer userConfFileReaderFile.Close()
+	userConfFileScanner := bufio.NewScanner(userConfFileReaderFile)
+
+	for userConfFileScanner.Scan() {
+		line := userConfFileScanner.Text()
+		if strings.TrimSpace(shellCode) != line {
+			_, err = tmpWriter.WriteString(fmt.Sprintf("%s\n", line))
+			if err != nil {
+				panic(err.Error())
+			}
+		}
+	}
+
+	// read from temp file and write it in userConf file
+	tmpFileReader := bufio.NewScanner(tmpFile)
+	userConfWriterFile, err := os.Create(user_conf_path)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer userConfWriterFile.Close()
+	userConfFileWriter := bufio.NewWriter(userConfWriterFile)
+	for tmpFileReader.Scan() {
+		line := tmpFileReader.Text()
+		userConfFileWriter.WriteString(fmt.Sprintf("%s\n", line))
+	}
+
+}
 
 func (sh FishShell) GetShellConfigPath() string {
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
 		panic(err.Error())
 	}
-
 	return path.Join(homeDir, ".config/fish/conf.d/envset.fish")
 }
 
